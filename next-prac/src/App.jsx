e5b4b60158edd273ac6fad4e5871f14f9717e6bb
jwy10 <1998dnjsdud@gmail.com>
2024-06-03 22:54:33 +0900

    동적기능연슨

--- next-prac/src/App.jsx
+++ next-prac/src/App.jsx
@@ -1,9 +1,10 @@
 
 import './App.css';
+import Post from './components/Post';
 
 function App() {
   return (
-    <h1>Hello! World!</h1>
+    <Post />
   );
 }
 
--- next-prac/src/components/Post.jsx
+++ next-prac/src/components/Post.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+const names = ["Maximilian", 'Manuel'];
+
 export default function Post() {
+const choosename = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div>
-      <p>Maximilan</p>
+      <p>{choosename}</p>
       <p>React.js is awesome!</p>
     </div>
   )
